db3bc4e990f81da9998f63b0118aba8088a8756e
John Cuba <ekubetsky@gmail.com>
2026-04-26 16:04:01 +0300

    feat: add open api

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,16 @@
 import { Elysia } from 'elysia';
+import { openapi } from '@elysia/openapi';
 import { viewModule } from './modules/view';
 import { chatModule } from './modules/chat';
 import { logger } from './lib/logger';
+import { isProd } from './lib/env';
 
 logger.debug('starting server')
 
 const app = new Elysia()
+  .use(openapi({
+    enabled: isProd()
+  }))
   .use(chatModule())
   .use(viewModule())
   .listen(process.env.PORT || 3000);
